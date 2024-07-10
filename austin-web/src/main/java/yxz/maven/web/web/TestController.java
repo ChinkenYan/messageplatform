ebd4136aba95f49927402015bc08b905cfdf4569
ChinkenYan <843465128@qq.com>
2024-07-10 19:03:32 +0800

    日志和工具包

--- austin-web/src/main/java/yxz/maven/web/web/TestController.java
+++ austin-web/src/main/java/yxz/maven/web/web/TestController.java
@@ -1,12 +1,15 @@
 package yxz.maven.web.web;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class TestController {
     @GetMapping("/test")
     public String Test(){
+        log.info("压力太大我好难受");
         return "hello,Austin";
     }
 }
